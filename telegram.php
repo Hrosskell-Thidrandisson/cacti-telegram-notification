14064ab65842a17925f4c83da6b581b8d4dfe365
Hrosskell-Thidrandisson <147601809+Hrosskell-Thidrandisson@users.noreply.github.com>
2023-10-13 12:37:02 +0200

    telegram.php

--- telegram.php
+++ telegram.php
@@ -1,11 +1,23 @@
 <?php
-	/*Telegram notification for cacti. Find admin_email function, send_mail line (/cacti/lib/functions.php about 4551 line) and instert*/
-	$TG_apiToken = "your token";
-	$TG_chatid ="your chat id";
-	/*$TG_text=$message;  */
-	$TG_text="testing"; 
-	$response = file_get_contents("https://api.telegram.org/bot$TG_apiToken/sendMessage?chat_id=$TG_chatid&text=$TG_text");
-    
+/*Telegram notification for cacti. 
+ 
+ Insert function send_telegram to /cacti/lib/functions.php and /cacti/plugin/monitor/poller.php
+
+ in /cacti/lib/functions.php find admin_email function and after send_mail line (about line 4551) insert send_telegram($message);
+
+ in plugin/monitor/poller.php find process_reboot_email function after process_send_email line (about line 390) instert: send_telegram($status . ' ' . $toutput);
+ and process_email funtions after process_send_email line (about line 568) instert: send_telegram($status . ' ' . $toutput); */
+
+
+send_telegram("hi world");
+
+function send_telegram($tg_text){
+	
+	$TG_apiToken = "6516941144:AAEbB3sJOiz3iTIEwzzkhIZzZMQphk6yVa0"; /* your telegram apiToken*/
+	$TG_chatid ="-4020774188"; /* your telegram chatid*/
+	$TG_message=$tg_text; 
+	$response = file_get_contents("https://api.telegram.org/bot$TG_apiToken/sendMessage?chat_id=$TG_chatid&text=$TG_message");
+}    
 ?>
 
 <html>
